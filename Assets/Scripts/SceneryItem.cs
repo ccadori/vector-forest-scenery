fa568d2c15ff7fafc5f3dcbc39ea14a494c0d2a8
ccadori <carloscadori.dev@gmail.com>
2021-12-11 20:30:44 -0300

    First commit

---
+++ Assets/Scripts/SceneryItem.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+
+public class SceneryItem : MonoBehaviour
+{
+    #region "Inspector"
+    public ParticleSystem _particle;
+    public Animator _animator;
+    public bool _randomizeSize;
+    public float _minSize = 0.85f;
+    public float _maxSize = 1.15f;
+    public SpriteRenderer[] colorChangeableRenderers;
+    #endregion
+
+    public void Start()
+    {
+        if (_particle != null)
+        {
+            _particle.gameObject.SetActive(false);
+        }
+
+        if (_randomizeSize)
+        {
+            RandomizeSize();
+        }
+    }
+
+    public void RandomizeSize()
+    {
+        float size = Random.Range(_minSize, _maxSize);
+        transform.localScale = new Vector2(size, size);
+    }
+
+    public void WindLeft()
+    {
+        _animator.SetTrigger("WindLeft");
+    }
+
+    public void WindRight()
+    {
+        _animator.SetTrigger("WindRight");
+    }
+
+    public void Shake()
+    {
+        _animator.SetTrigger("Shake");
+    }
+
+    public void SnowOn()
+    {
+        _animator.SetBool("Snow", true);
+    }
+
+    public void SnowOff()
+    {
+        _animator.SetBool("Snow", false);
+    }
+
+    public void TriggerParticle()
+    {
+        if (_particle != null)
+        {
+            _particle.gameObject.SetActive(true);
+            _particle.Stop();
+            _particle.Play();
+        }
+    }
+
+    public void SetRendererColor(Color color)
+    {
+        foreach (var renderer in colorChangeableRenderers)
+        {
+            renderer.color = color;
+        }
+    }
+}
---
+++ Assets/Scripts/Wind.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Trees
+{
+    public class Wind : MonoBehaviour
+    {
+        private SceneryItem[] trees;
+
+        public void Start()
+        {
+            trees = GameObject.FindObjectsOfType<SceneryItem>();
+            InvokeRepeating("WindLeft", 0, 4);
+        }
+
+        public void FixedUpdate()
+        {
+            if (Input.GetMouseButtonDown(00))
+            {
+                WindLeft();
+            }
+            else if (Input.GetMouseButtonDown(01))
+            {
+                WindRight();
+            }
+        }
+
+        public void WindLeft()
+        {
+            StartCoroutine(WindTrees((SceneryItem tree) => tree.WindLeft()));
+        }
+
+        public void WindRight()
+        {
+            StartCoroutine(WindTrees((SceneryItem tree) => tree.WindRight()));
+        }
+
+        public IEnumerator WindTrees(Action<SceneryItem> action)
+        {
+            float currentTime = 13;
+
+            List<SceneryItem> alreadyWind = new List<SceneryItem>();
+            while (alreadyWind.Count != trees.Length)
+            {
+                currentTime -= (Time.fixedDeltaTime * 10f);
+                yield return new WaitForFixedUpdate();
+
+                foreach (var tree in trees)
+                {
+                    if (!alreadyWind.Contains(tree) && tree.transform.position.x > currentTime)
+                    {
+                        action.Invoke(tree);
+                        alreadyWind.Add(tree);
+                    }
+                }
+            }
+        }
+    }
+}
